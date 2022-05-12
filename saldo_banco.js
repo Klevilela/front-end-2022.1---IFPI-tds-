337163fcd829d898295a1b794a17d04e61bb3bce
Kleberson <61327766+Klevilela@users.noreply.github.com>
2022-05-12 01:07:19 -0300

    Add files via upload

--- saldo_banco.js
+++ saldo_banco.js
@@ -1,55 +1,73 @@
-const saldo_inicial = 0
-const estado_inical = 'Saldo positvo'
 
-const saldo =  document.getElementById('saldo')
+//const estado_inicial = document.getElementById('estado').innerHTML= 'Saldo positvo'
+
+/*const saldo =  document.getElementById('saldo')
 const estado = document.getElementById('estado')
 
-const saque = parseInt(document.getElementById('saque').value)
-const deposito = parseInt(document.getElementById('depositar').value)
+const saque = document.getElementById('saque').value
+const deposito = document.getElementById('depositar').value
 
-const valor_saque = parseFloat(document.getElementById('valor_saldo').value)
-const valor_deposito = parseFloat(document.getElementById('valor_deposito').value)
+const valor_saque = parseFloat(document.getElementById('valor_saldo').value)*/
 
 function carrega_banco(){
-    document.saldo.addEventListener('click', saldo_e_situacao)
-    document.estado.addEventListener('click', saldo_e_situacao)
+    document.getElementById('saldo').innerHTML = 'Saldo: 0 R$'
+    document.getElementById('estado').innerHTML = 'Estado: Saldo Positivo'
 
-    document.valor_saque.addEventListener('click', sacar)
-    document.valor_deposito.addEventListener('click', depositar)
-
+    document.getElementById('valor_deposito').addEventListener('click', depositar)
     document.getElementById('depositar').addEventListener('click', depositar)
+
+
+    document.getElementById('saque').addEventListener('click', sacar)
+    document.getElementById('valor_saque').addEventListener('click', sacar)
+
     document.getElementById('sacar').addEventListener('click', sacar)
+
 }
 
 function depositar(){
-    const saldo = 0
-    const aumentar_saldo = saldo_inicial + valor_deposito
-    return aumentar_saldo
-    //document.saldo.innerHTML = `${aumentar_saldo}`
+    //var saldo_atual = aumentar_saldo
+    var valor_deposito = parseInt(document.getElementById('valor_deposito').value)
+    //var soma = valor_deposito
+    var aumentar_saldo = valor_deposito
 
-    document.valor_saque.innerHTML = aumentar_saldo
-    document.saldo.innerHTML = `<h1>${valor_saque} R$</h1>`
+    //aumentar_saldo = parseFloat(valor_deposito)
+        //aumentar_saldo =+ saldo_atual
+    //document.getElementById('depositar') = aumentar_saldo
+    document.getElementById('saldo').innerHTML = aumentar_saldo  + ' R$'
+    
+
+
+    //document.valor_saque.innerHTML = aumentar_saldo
+    //var saldo =+ aumentar_saldo
+    //document.getElementById('saldo').innerHTML = saldo
 
 }
 
 function sacar(){
-    const saque = saldo - valor_saq
-    document.saldo.innerHTML = `${aumentar_saldo}`
+    var saldo = parseInt(document.getElementById('saldo').value)
+    var valor_saque = parseInt(document.getElementById('valor_saque').value)
+    var estado = document.getElementById('estado')
 
-    if (saldo >= 0 && valor_saque <= saldo && estado === 'Saldo posiitvo'){
-        const saque = saldo - valor_saque 
+    var saque = saldo - valor_saque
+
+    //document.getElementById('saldo').innerHTML = saque
+
+    if (saldo > 0 && valor_saque <= saldo){
+        saque = saldo - valor_saque
+        document.saldo.innerHTML = saque + 'R$' 
     }
-    else{document.estado.innerHTML = 'saldo negativo'}
+    else{document.estado.innerHTML = 'Saldo negativo'}
 
-    document.getElementById
 }
 
 
-function saldo_e_situacao(){
-    document.saldo.innerHTML = `<h2>${saldo_inicial} R$</h2>`
-    document.estado_inical.innerHTML = `<h2>${saldo_inicial}</h2>`
+/*function saldo_e_situacao(){
+    
+    document.getElementById('saldo').innerHTML = `${saldo_inicial}  'R$`
+    document.getElementById('estado').innerHTML = '<h2>Saldo Positivo</h2>'
 }
+
 
 // document.getElementById('saldo').innerHTML = `0`
 // document.saldo.write = `<h1>${saldo_inicial}</h1>`
-// document.estado_inical.write = `${estado}`
+// document.estado_inical.write = `${estado}`*/
